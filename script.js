9adc599fb514399f62c68135e8e9ac10a04db838
Thayner Kesley <125166594+ThaynerKesley@users.noreply.github.com>
2023-09-29 11:15:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,30 @@
-function generatePassword() {
-  const length = document.getElementById("password-length").value;
-  const includeUppercase = document.getElementById("include-uppercase").checked;
-  const includeLowercase = document.getElementById("include-lowercase").checked;
-  const includeNumbers = document.getElementById("include-numbers").checked;
-  const includeSymbols = document.getElementById("include-symbols").checked;
+const inputText = document.getElementById("inputText");
+const encodeButton = document.getElementById("encodeButton");
+const decodeButton = document.getElementById("decodeButton");
+const outputText = document.getElementById("outputText");
+const copyButton = document.getElementById("copyButton");
 
-  const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
-  const numbers = "0123456789";
-  const symbols = "!@#$%^&*()_+=-{}[]|:;<>,.?/";
+encodeButton.addEventListener("click", () => {
+  const encodedText = btoa(inputText.value);
+  outputText.value = encodedText;
+  toggleCopyButton(true);
+});
 
-  let characters = "";
-  let password = "";
+decodeButton.addEventListener("click", () => {
+  const decodedText = atob(inputText.value);
+  outputText.value = decodedText;
+  toggleCopyButton(true);
+});
 
-  if (includeUppercase) {
-    characters += uppercaseLetters;
-  }
+outputText.addEventListener("input", () => {
+  toggleCopyButton(false);
+});
 
-  if (includeLowercase) {
-    characters += lowercaseLetters;
-  }
-
-  if (includeNumbers) {
-    characters += numbers;
-  }
-
-  if (includeSymbols) {
-    characters += symbols;
-  }
-
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    password += characters.charAt(randomIndex);
-  }
-
-  const generatedPasswordInput = document.getElementById("generated-password");
-  const copyButton = document.getElementById("copy-button");
-
-  generatedPasswordInput.value = password;
-  copyButton.disabled = false;
-}
-
-function copyPassword() {
-  const generatedPasswordInput = document.getElementById("generated-password");
-
-  generatedPasswordInput.select();
-  generatedPasswordInput.setSelectionRange(0, 99999);
+copyButton.addEventListener("click", () => {
+  outputText.select();
   document.execCommand("copy");
+});
 
-  const copyButton = document.getElementById("copy-button");
-  copyButton.innerText = "Senha Copiada!";
-  copyButton.disabled = true;
-  setTimeout(() => {
-    copyButton.innerText = "Copiar Senha";
-    copyButton.disabled = false;
-  }, 3000);
+function toggleCopyButton(enabled) {
+  copyButton.disabled = !enabled;
 }
